5bebbd9db89ebad8893180392306732822c5c913
esosa89 <obakpoloresosa@yahoo.com>
2023-01-23 14:43:44 -0800

    main.h

--- main.h
+++ main.h
@@ -40,32 +40,32 @@ struct fmt
 typedef struct fmt fmt_t;
 
 int _printf(const char *format, ...);
-int handle_print(const char *fmt, int *i
+int handle_print(const char *fmt, int *i,
 		va_list list, char buffer[], int flags, int width, int precision, int size);
 
 /****************** FUNCTIONS ******************/
 
 /* Funtions to print chars and strings */
 int print_char(va_list types, char buffer[],
-	       int flags, int width, int precision, int size);
+	int flags, int width, int precision, int size);
 int print_string(va_list types, char buffer[],
-		 int flags, int width, int precision, int size);
+		int flags, int width, int precision, int size);
 int print_percent(va_list types, char buffer[],
-		  int flags, int width, int precision, int size);
+		int flags, int width, int precision, int size);
 
 /* Functions to print numbers */
 int print_int(va_list types, char buffer[],
-	      int flags, int width, int precision, int size);
+	int flags, int width, int precision, int size);
 int print_binary(va_list types, char buffer[],
-		 int flags, int width, int precision, int size);
+		int flags, int width, int precision, int size);
 int print_unsigned(va_list types, char buffer[],
-		   int flags, int width, int precision, int size);
+		int flags, int width, int precision, int size);
 int print_octal(va_list types, char buffer[],
 		int flags, int width, int precision, int size);
 int print_hexadecimal(va_list types, char buffer[],
-		      int flags, int width, int precision, int size);
+		int flags, int width, int precision, int size);
 int print_hexa_upper(va_list types, char buffer[],
-		     int flags, int width, int precision, int size);
+		int flags, int width, int precision, int size);
 
 int print_hexa(va_list types, char map_to[],
 	char buffer[], int flags, char flag_ch, int width, int precision, int size);
